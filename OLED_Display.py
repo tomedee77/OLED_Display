556a0a7348188194b36ee1706ef2d2c4b994d0f4
tomedee77 <tomedee77@gmail.com>
2025-09-21 10:59:49 +0100

    Update OLED_Display.py

--- OLED_Display.py
+++ OLED_Display.py
@@ -23,13 +23,13 @@ LIVE_LABELS = ["MAP", "AFR", "CLT", "MAT"]
 TEST_LABELS = ["RPM", "TPS", "AFR", "Coolant", "IAT"]
 TEST_VALUES = ["1000", "12.5%", "14.7", "90°C", "25°C"]
 
-font_small = ImageFont.load_default()
+font_small = ImageFont.load_default(15)
 try:
     font_large = ImageFont.truetype(
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 16
+        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30
     )
 except:
-    font_large = ImageFont.load_default()
+    font_large = ImageFont.load_default(30)
 
 # ----------------------------
 # CLEAR OLD LOGS ON STARTUP
